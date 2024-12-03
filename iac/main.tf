39b183689fc9eb04753cb50dc37a7d15661aae94
Pierre-Alexandre35 <46579114+Pierre-Alexandre35@users.noreply.github.com>
2024-12-03 11:37:24 +0100

    (infra) terraform files for compute engine DB

---
+++ iac/main.tf
@@ -0,0 +1,75 @@
+# Google provider configuration
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# VPC Network
+resource "google_compute_network" "default" {
+  name = "default-network"
+}
+
+# Firewall rule for PostgreSQL (port 5432)
+resource "google_compute_firewall" "allow_postgres" {
+  name    = "allow-postgres"
+  network = google_compute_network.default.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Restrict this to specific IPs for better security
+}
+
+# Firewall rule for SSH (port 22)
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.default.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Restrict this to your IP range
+  target_tags   = ["ssh"]
+}
+
+# Compute Engine instance for PostgreSQL
+resource "google_compute_instance" "postgres_instance" {
+  name         = "postgres-instance"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  tags = ["ssh"] # Ensures SSH firewall rule applies to this instance
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/family/debian-11"
+      size  = 50 # Disk size in GB
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.default.self_link
+
+    access_config {
+      # Enables external IP
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+    #! /bin/bash
+    apt-get update
+    apt-get install -y postgresql
+    systemctl enable postgresql
+    systemctl start postgresql
+  EOT
+}
+
+# Output the external IP address of the instance
+output "postgres_external_ip" {
+  value = google_compute_instance.postgres_instance.network_interface[0].access_config[0].nat_ip
+  description = "External IP address of the PostgreSQL instance"
+}
---
+++ iac/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  default     = "my-sandbox-441808"
+}
+
+variable "region" {
+  description = ""
+  default     = "europe-west9"
+}
+
+variable "zone" {
+  description = ""
+  default     = "europe-west9-b"
+}
+
